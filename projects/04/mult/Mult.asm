b3c8ee616b981b86864f41fb1aa8268e2b89412f
Your NameKJhari <harikrishnankj555@gmail.com>
2022-12-31 18:29:38 -0800

    Mult.asm done

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,51 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+
+
+@R1                   // load r1 into memory
+D=M
+
+@i
+M=D                   // load r1 as i into mem16
+
+@p
+M=0                  // load mem17 = 0 for partial sum
+
+(LOOP)
+    @i
+    D=M
+    @LDREG          // if i!=0 // loop
+    D; JEQ 
+
+    @R0
+    D = M           // D = R0
+    @p
+    M = M + D       // Add R0 R1 times 
+    @i
+    M= M - 1       // decrement i
+    @LOOP
+    0;JMP
+
+(LDREG)
+    @p             //load p into D 
+    D = M
+    @R2            // load addr of destination register R2
+    M = D          // load product into R2
+    @END           // Jump to END loop
+    0;JMP
+
+(END)
+    @END
+    0;JMP
+
+
+
+
+
+
+
+
+
+ 
